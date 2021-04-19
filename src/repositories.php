8fb6d9bde1b02f51f360a6ebc3d4ec1f093a8886
火子 <yxyong@126.com>
2021-04-19 10:48:20 +0800

    init

--- src/repositories.php
+++ src/repositories.php
@@ -5,7 +5,6 @@ use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
   $containerBuilder->addDefinitions([
-    \Wanphp\Components\Category\Domain\TagsInterface::class => \DI\autowire(\Wanphp\Components\Category\Repositories\TagsRepository::class),
     \Wanphp\Plugins\About\Domain\AboutInterface::class => \DI\autowire(\Wanphp\Plugins\About\Repositories\AboutRepository::class)
   ]);
 };
